f0da75ce0aa0aabd7098fb6b8d18f7e1add84437
Neil Casas <neilalfonzcasas@gmail.com>
2025-01-22 22:32:32 +0800

    created list component

--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -4,6 +4,8 @@ import { StaticImageData } from "next/image";
 import Image from "next/image";
 import styles from "./List.module.scss";
 import { useState } from "react";
+import { Subheader } from "./Subheader";
+import MetaLogo from "../../../public/meta-logo.webp";
 
 type ListItemProps = {
   title: string;
@@ -14,12 +16,13 @@ type ListItemProps = {
   href?: string;
 };
 
-export const Item: React.FC<ListItemProps> = (props: ListItemProps) => {
-  const [open, setOpen] = useState(false);
+const Item: React.FC<ListItemProps> = (props: ListItemProps) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleOpen = () => {
-    setOpen(!open);
+    setIsOpen(!isOpen);
   };
+
   return (
     <div className={styles.listItem}>
       <div className={styles.imgContainer}>
@@ -30,13 +33,65 @@ export const Item: React.FC<ListItemProps> = (props: ListItemProps) => {
           <strong>{props.title}</strong>
           <p>{props.subtitle}</p>
           {props.description && (
-            <div className={`${styles.description} ${open ? styles.show : ""}`}>
+            <div
+              className={`${styles.description} ${isOpen ? styles.show : ""}`}
+            >
               {props.description}
             </div>
           )}
         </div>
         <p>{props.years}</p>
       </div>
+    </div>
+  );
+};
+
+export const testProps: ListItemProps[] = [
+  {
+    title: "TomasinoWeb",
+    subtitle: "Frontend Developer",
+    years: "2024-2025",
+    image: MetaLogo,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida magna eu felis consequat, et lacinia neque hendrerit. Donec et arcu mauris. Pellentesque at ultricies tellus, a tempor ex. Etiam rutrum eu lectus sed fringilla. Nunc rhoncus est sed nunc cursus, vel venenatis justo aliquam. Etiam id lacus sit amet purus cursus tincidunt a at leo. Praesent egestas condimentum porttitor. Ut a mollis lacus.",
+  },
+  {
+    title: "TomasinoWeb",
+    subtitle: "Frontend Developer",
+    years: "2024-2025",
+    image: MetaLogo,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida magna eu felis consequat, et lacinia neque hendrerit. Donec et arcu mauris. Pellentesque at ultricies tellus, a tempor ex. Etiam rutrum eu lectus sed fringilla. Nunc rhoncus est sed nunc cursus, vel venenatis justo aliquam. Etiam id lacus sit amet purus cursus tincidunt a at leo. Praesent egestas condimentum porttitor. Ut a mollis lacus.",
+  },
+  {
+    title: "TomasinoWeb",
+    subtitle: "Frontend Developer",
+    years: "2024-2025",
+    image: MetaLogo,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida magna eu felis consequat, et lacinia neque hendrerit. Donec et arcu mauris. Pellentesque at ultricies tellus, a tempor ex. Etiam rutrum eu lectus sed fringilla. Nunc rhoncus est sed nunc cursus, vel venenatis justo aliquam. Etiam id lacus sit amet purus cursus tincidunt a at leo. Praesent egestas condimentum porttitor. Ut a mollis lacus.",
+  },
+];
+
+type ListProps = {
+  items: ListItemProps[];
+  title: string;
+};
+
+export const List: React.FC<ListProps> = (props: ListProps) => {
+  return (
+    <div className={styles.list}>
+      <Subheader text={props.title}></Subheader>
+      {props.items.map((item, key) => (
+        <Item
+          key={key}
+          title={item.title}
+          subtitle={item.subtitle}
+          years={item.years}
+          image={item.image}
+          description={item.description}
+        />
+      ))}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 import { Hero } from "./components/Hero";
 import { About } from "./components/About";
-import { Item } from "./components/List";
+import { testProps, List } from "./components/List";
 import styles from "./page.module.scss";
-import Meta from "../../public/meta-logo.webp";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero></Hero>
       <About></About>
-      <Item
-        image={Meta}
-        title="Software Engineer"
-        subtitle="Meta"
-        years="2023-2024"
-        description="hello"
-      ></Item>
+      <List items={testProps} title="experience"></List>
+      <List items={testProps} title="education"></List>
     </main>
   );
 }
